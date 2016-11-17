6a2a1c3deef6857ec4a5f4ddab073bf16bd1bbae
BuildTools <unconfigured@null.spigotmc.org>
2016-11-17 06:58:32 +0100

    ...

--- src/me/numixe/zombieinvasion/entities/Disguiser.java
+++ src/me/numixe/zombieinvasion/entities/Disguiser.java
@@ -1,7 +1,5 @@
 package me.numixe.zombieinvasion.entities;
 
-import static me.numixe.zombieinvasion.ZombieInvasion.pl;
-
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
@@ -19,35 +17,39 @@ public class Disguiser implements Listener {
 	//@SuppressWarnings("deprecation")
 	//private static MobDisguise zombie = new MobDisguise(DisguiseType.ZOMBIE, true);
 	
-	@SuppressWarnings("deprecation")
 	public static void setVillager(Player p) {
-	    pl.getScoreboard().vill.addPlayer(p);
+		
+	    //pl.getScoreboard().vill.addPlayer(p);
         //api.disguiseToAll(p, villager);
+		
+		/*
+		 *  La score board non si deve basare sul numero di villager e zombie in generale
+		 *  Ma solo quelli presenti in gioco, dunque si aggiorna in automatico secondo il count fornito dalla lobby
+		 */
+		
 	    Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(),"odisguise " + p.getName() + " villager");
         ScreenAPI.sendMessage(p, "\u00A7a\u00A7lSei un Villager!");
         p.getInventory().setItem(0, Item.villSword);
 	}
 	
-	@SuppressWarnings("deprecation")
 	public static void setZombie(Player p) {	
     
-		pl.getScoreboard().zomb.addPlayer(p);
+		//pl.getScoreboard().zomb.addPlayer(p);
         //api.disguiseToAll(p, zombie);
         ScreenAPI.sendMessage(p, "\u00A7a\u00A7lSei uno Zombie!");      
         Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(),"odisguise " + p.getName() + " zombie infected");
         p.getInventory().setItem(0, Item.zombSword);
 	}
 	
-	@SuppressWarnings("deprecation")
 	public static void setNull(Player p) {
 		
 		if (p == null)
 			return;
 		
-		if (pl.getScoreboard().vill.getPlayers().contains(p))
+		/*if (pl.getScoreboard().vill.getPlayers().contains(p))
 			pl.getScoreboard().vill.removePlayer(p);
 		if (pl.getScoreboard().zomb.getPlayers().contains(p))
-			pl.getScoreboard().zomb.removePlayer(p);
+			pl.getScoreboard().zomb.removePlayer(p);*/
 		p.getInventory().clear();
 		//api.undisguiseToAll(p);
 		Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "undisguise " + p.getName());
